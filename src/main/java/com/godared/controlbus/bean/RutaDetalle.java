97c8cf96e7b773c6e6fe415d78ad62a90e3cc678
RONALD MAMANI NINAJA <RONALD MAMANI NINAJA>
2017-03-23 09:26:23 -0500

    actualizando para recuperar el server

--- src/main/java/com/godared/controlbus/bean/RutaDetalle.java
+++ src/main/java/com/godared/controlbus/bean/RutaDetalle.java
@@ -3,7 +3,6 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedStoredProcedureQueries;
 import javax.persistence.NamedStoredProcedureQuery;
